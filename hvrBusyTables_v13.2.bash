3338971793dec27494e1bc15f475307b20fc7b52
cjtijerina <35704515+cjtijerina@users.noreply.github.com>
2023-06-26 13:19:29 -0400

    Update hvrBusyTables_v13.2.bash

--- hvrBusyTables_v13.2.bash
+++ hvrBusyTables_v13.2.bash
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 #######################################################################################
+## Creator: ctijerina 
+## Creation time: 2018 changes thru 2022
+## Legend: CDC = change data capture - capturing inserts, updates and deletes
+##         Client: the user of this script - can be you too.
+## This script runs on: Linux, Mac Terminal, Windows(but must use WSL bash - it's a linux sub system on windows)
+##
 ## The goal is to parse the hvr.out file for all CAPTURE activity for a given run time.
 ## If no capture key word then integrate processed rows will be calculated
 ## The client will be asked to setup a Capture and allow it to run for 24+ hours (2 days)
@@ -13,7 +19,7 @@
 # 1. Total # of Application Tables in your CDC Replication/PipeLine- 
 # 2. Total # of Changed Rows - changed by Insert, Update or Delete - broTip: this is CDC
 # 3. A list of all tables with the total amount of changed rows
-# 4. Small - Medium - Busy groups based on calculation  sum/tableTotalchanged*100
+# 4. Post programatically analyzing: Small - Medium - Busy groups based on calculation  sum/tableTotalchanged*100
 #### C.BENEFITS - what to do with the output from this script
 # 1. Determine how to break up parallize your downstream pipe lines for better thru put
 # 2. Use this data to proavtively determine capacity and when will it be time to add more capacity
